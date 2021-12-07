a745502a8adf33a73a1c96a867bf05923f1cb804
davidgolay <74565736+davidgolay@users.noreply.github.com>
2021-12-07 20:18:37 +0100

    feat: Animation de transition entre les téléportations
    Co-Authored-By: MathisPct <57356459+MathisPct@users.noreply.github.com>

--- Assets/Scripts/Teleportation/TeleportAwareness.cs
+++ Assets/Scripts/Teleportation/TeleportAwareness.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Assets.Scripts.XRExtension
@@ -13,12 +15,27 @@ namespace Assets.Scripts.XRExtension
     /// </summary>
     public class TeleportAwareness : BaseTeleportationInteractable
     {
+
+        [SerializeField] private Transition screenFade;
+
         [SerializeField] private TeleportationManager teleportationManager;
 
         /// <summary>
         /// Transform of destination
         /// </summary>
         private Transform transformOfDestination;
+
+        private void Awake()
+        {
+            base.Awake();
+            screenFade = FindObjectOfType<Transition>();
+        }
+
+        protected override void OnSelectExited(SelectExitEventArgs args)
+        {
+            if (teleportTrigger == TeleportTrigger.OnSelectExited)
+                StartCoroutine(FadeSequence(base.OnSelectExited, args));
+        }
 
         public Transform TransformOfDestination { get => transformOfDestination; set => transformOfDestination = value; }
 
@@ -58,6 +75,27 @@ namespace Assets.Scripts.XRExtension
         {
             var teleportAware = raycastHit.collider.gameObject.GetComponentInParent<TeleportAwareness>();
             return teleportAware;
-        } 
+        }
+
+        /// <summary>
+        /// Disable interation and fade to black (transition).
+        /// Wait, then do the teleport stuff, fade from black to transparent transition, enable interaction
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private IEnumerator FadeSequence<T>(UnityAction<T> action, T args)
+            where T : BaseInteractionEventArgs
+        {
+            //disable interation and fade to black transition
+            interactionManager.enabled = false;
+            screenFade.FadeIn();
+            // Wait, then do the teleport stuff, fade from black to transparent transition, enable interaction
+            yield return new WaitForSeconds(screenFade.DurationFadeIn);
+            action.Invoke(args);
+            screenFade.FadeOut();
+            interactionManager.enabled = true;
+        }
     }
 }
